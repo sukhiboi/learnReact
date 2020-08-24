d08aa5fd4757a95cd83c1d22874acace7da14af7
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-08-24 18:25:06 +0530

    created HelloWorld component

--- learnings/src/HelloWorld.js
+++ learnings/src/HelloWorld.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function App() {
+function HelloWorld() {
   return <div>Hello World</div>;
 }
 
-export default App;
+export default HelloWorld;
--- learnings/src/index.js
+++ learnings/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import HelloWorld from './HelloWorld';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
