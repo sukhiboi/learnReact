29a1d7dba6375b3215f5161cf1cd352e69a5a91e
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-08-25 19:41:01 +0530

    showing the current player

--- learnings/src/ticTacToe/TicTacToe.js
+++ learnings/src/ticTacToe/TicTacToe.js
@@ -35,18 +35,21 @@ class TicTacToe extends React.Component {
     const fieldIds = Object.keys(this.state.board);
     const fieldValues = fieldIds.map(field => this.state.board[field]);
     return (
-      <div className='board'>
-        {fieldValues.map((fieldValue, id) => {
-          const fieldId = ++id;
-          return (
-            <Field
-              symbol={fieldValue}
-              id={fieldId}
-              key={fieldId}
-              onClick={this.updateGame}
-            />
-          );
-        })}
+      <div>
+        <div className='board'>
+          {fieldValues.map((fieldValue, id) => {
+            const fieldId = ++id;
+            return (
+              <Field
+                symbol={fieldValue}
+                id={fieldId}
+                key={fieldId}
+                onClick={this.updateGame}
+              />
+            );
+          })}
+        </div>
+        <span>{this.state.currentSymbol}'s turn</span>
       </div>
     );
   }
