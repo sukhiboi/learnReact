65cd470d8a7d8fffb8f3903519a5aee7d206fecc
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-04 17:03:26 +0530

    added pokemon api with timer
    new pokemon every second

--- learnings/src/hooks/useEffect.js
+++ learnings/src/hooks/useEffect.js
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from 'react';
 
+const fetchPokemonDetails = (id, callback) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(({ name, sprites }) => ({ name, img: sprites.front_default }))
+    .then(data => callback(data));
+};
+
 export default props => {
-  const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(1);
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    fetchPokemonDetails(seconds, data => setPokemon(data));
+  }, [seconds]);
 
   useEffect(() => {
     document.title = `${seconds} seconds passed`;
   });
 
   useEffect(() => {
-    const id = setInterval(() => setSeconds(seconds => seconds + 1), 1000);
+    const id = setInterval(() => setSeconds(seconds => seconds + 1), 3000);
     return () => clearInterval(id);
   }, []);
 
-  return <p>{seconds} </p>;
+  return (
+    <div>
+      <p>{seconds} </p>
+      {pokemon ? (
+        <div>
+          <p>{pokemon.name}</p>
+          <img src={pokemon.img} alt={pokemon.name} />
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
