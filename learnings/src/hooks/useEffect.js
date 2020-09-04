7ce165b4cb0aaea51ae56e5c0542003a741cbfa7
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-04 16:55:18 +0530

    used 2 useEffects

--- learnings/src/hooks/useEffect.js
+++ learnings/src/hooks/useEffect.js
@@ -4,6 +4,10 @@ export default props => {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
+    document.title = `${seconds} seconds passed`;
+  });
+
+  useEffect(() => {
     const id = setInterval(() => setSeconds(seconds => seconds + 1), 1000);
     return () => clearInterval(id);
   }, []);
